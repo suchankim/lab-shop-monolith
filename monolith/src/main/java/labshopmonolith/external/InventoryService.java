d027ce86c1fee338d397e5f54e3024e7e881c4dd
suchankim <jaekim4@hanmail.net>
2023-04-08 19:39:24 +0900

    commit

--- monolith/src/main/java/labshopmonolith/external/InventoryService.java
+++ monolith/src/main/java/labshopmonolith/external/InventoryService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "monolith", url = "${api.url.monolith}")
+@FeignClient(name = "inventory", url = "${api.url.inventory}")
 public interface InventoryService {
     @RequestMapping(
         method = RequestMethod.PUT,
